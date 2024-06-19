a45399595d906b26c08f03022651fd89b4f7111d
oTsimpikakis <odisschannel@gmail.com>
2024-06-19 23:49:51 +0300

    import pdf start

---
+++ tests/Unit/ImportManualTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Support\Facades\Storage;
+use Smalot\PdfParser\Parser;
+use Tests\TestCase;
+
+class ImportManualTest extends TestCase
+{
+    /** @test */
+    public function a_pdf_can_be_imported() {
+        $file = Storage::get('public/Traffic Jam.pdf');
+        ini_set('memory_limit', '2G');
+
+        $ml = ini_get('memory_limit');
+
+        // dd($ml);
+
+        $parser = new Parser();
+
+        $pdf = $parser->parseFile(storage_path('app/public/Traffic Jam.pdf'));
+
+        $text = $pdf->getText();
+        $details = $pdf->getDetails();
+
+        dd($text, $details);
+
+        $this->assertTrue(true);
+    }
+}
